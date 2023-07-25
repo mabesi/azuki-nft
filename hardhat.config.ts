72752fd7410d23e6ea5a5f14c8e99b9154ad20bd
Plinio Mabesi <pliniomabesi@gmail.com>
2023-07-25 19:53:20 -0300

    New ABI and hardhat config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -9,6 +9,11 @@ dotenv.config();
 // Successfully verified contract MabesiAzukiNFT on Etherscan.
 // https://testnet.snowtrace.io/address/0xa018Fc9DecA4A08661Cb214f666538ED988fdfB3#code
 
+// Mabesi Azuki NFT contract was deployed at 0x55E22399a11B33ae50408eC4d564652c395DB7e7
+// Successfully verified contract MabesiAzukiNFT on Etherscan.
+// https://testnet.snowtrace.io/address/0x55E22399a11B33ae50408eC4d564652c395DB7e7#code
+// With Contract URI and LOGO
+
 const config: HardhatUserConfig = {
   solidity: "0.8.19",
   networks: {
